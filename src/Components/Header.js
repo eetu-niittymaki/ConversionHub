d53b99877f191c6a3230f16fc747d1a997e18d40
eetu-niittymaki <eetu945@gmail.com>
2024-07-30 19:28:20 +0300

    Add styling to header title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,9 @@ import "./Header.css"
 const Header = ({children, sendToParent}) => {
     return(
         <div className="main">
-            <span style={{color:"black"}}>Conversion</span><span style={{color:"white"}}>Hub</span>
+            <span onClick={() => sendToParent("")}>
+                <span className="titleStart">Conversion</span><span className="titleEnd">Hub</span>
+            </span>
             <div className="container">
                 {children.map(child => (
                     <h3 className="children" onClick={() => sendToParent(child.props.children)}>{child}</h3>
