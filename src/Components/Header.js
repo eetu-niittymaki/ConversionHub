f1c5ed524f10ff4d5166d800b79d98317c889513
eetu-niittymaki <eetu945@gmail.com>
2024-08-25 18:58:54 +0300

    Add small layout changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css"
 
 const Header = ({children, sendToParent}) => {
+    const [selectedChild, setSelectedChild] = useState(null)
+
+    const handleClick = (child) => {
+        setSelectedChild(child)
+        sendToParent(child)
+    }
+
     return(
         <div className="main">
-            <span onClick={() => sendToParent("")}>
+            <span onClick={() => handleClick("")}>
                 <span className="titleStart">Conversion</span>
-                <img src={process.env.PUBLIC_URL + "/images/arrow.png"} alt="Arrow" className= "titleImg"/>
+                <img src={process.env.PUBLIC_URL + "/images/arrow.png"} alt="Arrow" className="titleImg"/>
                 <span className="titleEnd">Hub</span>
             </span>
             <div className="container">
                 {children.map(child => (
-                    <h3 className="children" onClick={() => sendToParent(child.props.children)}>{child}</h3>
+                    <h3 key={child.props.children} 
+                        className={`children ${selectedChild === child.props.children ? 'active' : ''}`}
+                        onClick={() => handleClick(child.props.children)}
+                    >
+                        {child}
+                    </h3>
                 ))}
             </div>
         </div>
